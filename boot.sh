d48d57bd770b9c83c3aeec97e06c7d2a35d89af9
FK <francis.korning@cso.ie>
2025-03-03 09:10:24 +0000

    inception.sh

--- boot.sh
+++ boot.sh
@@ -12,17 +12,20 @@ apt update
 # aws-cli
 snap install aws-cli --classic
 
+# apache httpd
+snap install httpd --classic
+
 
 # banner (motd) 
-curl  https://raw.githubusercontent.com/korningf/vbox-cso-linux/refs/heads/main/motd > /etc/motd
+curl  https://raw.githubusercontent.com/korningf/vbox-aws-linux/refs/heads/main/motd > /etc/motd
 
 # constants
 REGION=us-east-1
 
 # variables
 AWS_EC2_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
-AWS_EC2_NAME=$(aws ec2 describe-tags --region $REGION --filters "Name=resource-id,Values=$AWS_EC2_ID" "Name=key,Values=Name" --output text | cut -f5)
+#AWS_EC2_NAME=$(aws ec2 describe-tags --region $REGION --filters "Name=resource-id,Values=$AWS_EC2_ID" "Name=key,Values=Name" --output text | cut -f5)
 
 echo $AWS_EC2_ID >> /etc/aws_ec2_id
-echo $AWS_EC2_NAME >> /etc/aws_ec2_name
+#echo $AWS_EC2_NAME >> /etc/aws_ec2_name
 
